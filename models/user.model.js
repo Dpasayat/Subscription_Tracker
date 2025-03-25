ec80c318848ab511b559d7aa617fb62a0dcfdf72
Dhiraj Kumar Pasayat <126373457+Dpasayat@users.noreply.github.com>
2025-03-25 11:36:39 +0530

    User and Subscription models, Error handling middleware

--- models/user.model.js
+++ models/user.model.js
@@ -1,34 +1,35 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: { 
-    type: String, 
-    required: [true, 'User name is required'],
-    trim: true,
-    minLength: 2,
-    maxLength: 50,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "User name is required"],
+      trim: true,
+      minLength: 2,
+      maxLength: 50,
     },
     email: {
-        type: String,
-        required: [true, 'User email is required'],
-        trim: true,
-        minLength: 5,
-        maxLength: 50,
-        unique: true,
-        lowercase: true,
-        match: [/\S+@\S+\.\S+/, 'Please enter a valid email']
-
+      type: String,
+      required: [true, "User email is required"],
+      trim: true,
+      minLength: 5,
+      maxLength: 50,
+      unique: true,
+      lowercase: true,
+      match: [/\S+@\S+\.\S+/, "Please enter a valid email"],
     },
     password: {
-        type: String,
-        required: [true, 'User password is required'],
-        minLength: 6,
-    }, 
-},
-{timestamps: true});
+      type: String,
+      required: [true, "User password is required"],
+      minLength: 6,
+    },
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
 
-   
+//{name:Dkp,email:dkp@gmail.com,password:pass}
